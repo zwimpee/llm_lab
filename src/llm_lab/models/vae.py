30745f6bf59b5fdc835f08ddb52bd27a2be25100
zwimpee <zwimpee@angelo.edu>
2024-03-14 10:32:11 -0500

    Add vae.py

---
+++ src/llm_lab/models/vae.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class VAE(nn.Module):
+    def __init__(self, input_dim, latent_dim):
+        super(VAE, self).__init__()
+        # Encoder
+        self.fc1 = nn.Linear(input_dim, 512)
+        self.fc2_mean = nn.Linear(512, latent_dim)
+        self.fc2_logvar = nn.Linear(512, latent_dim)
+        # Decoder
+        self.fc3 = nn.Linear(latent_dim, 512)
+        self.fc4 = nn.Linear(512, input_dim)
+    
+    def encode(self, x):
+        h1 = F.relu(self.fc1(x))
+        return self.fc2_mean(h1), self.fc2_logvar(h1)
+    
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+    
+    def decode(self, z):
+        h3 = F.relu(self.fc3(z))
+        return torch.sigmoid(self.fc4(h3))
+    
+    def forward(self, x):
+        mu, logvar = self.encode(x.view(-1, 784))
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z), mu, logvar
+
+def vae_loss(recon_x, x, mu, logvar):
+    BCE = F.binary_cross_entropy(recon_x, x.view(-1, 784), reduction='sum')
+    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+    return BCE + KLD
